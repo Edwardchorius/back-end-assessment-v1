66262938054a07949561c62fce05e7ef6e655e37
eduard vaklinov <eduard.vaklinov@abx.com>
2022-04-11 08:51:06 +0300

    Initial setup of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('EmailGenerator')
+    .setDescription(
+      'Generating emails based on passed in input values and expression containing the pattern',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(9443);
 }
 bootstrap();
