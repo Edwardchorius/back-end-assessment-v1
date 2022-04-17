9ee12b683e9babe535fba023aa2d64c8d49778a8
eduard vaklinov <eduard.vaklinov@abx.com>
2022-04-17 22:45:26 +0300

    Added try catch block

--- src/domain/services/EmailAddressGenerator.ts
+++ src/domain/services/EmailAddressGenerator.ts
@@ -13,89 +13,96 @@ export class EmailAddressGenerator {
     inputs: EmailAddressGenerationInput[];
     expression: string;
   }): EmailAddressGeneratedOutput {
-    const { inputs, expression } = requestModel;
-    const resultEmailAddress: string[] = [];
+    try {
+      const { inputs, expression } = requestModel;
+      const resultEmailAddress: string[] = [];
 
-    const inputsToValues = inputs.reduce((acc, currentInput) => {
-      return {
-        ...acc,
-        [currentInput.name]: currentInput.value,
-      };
-    }, {}) as Record<string, string>;
+      const inputsToValues = inputs.reduce((acc, currentInput) => {
+        return {
+          ...acc,
+          [currentInput.name]: currentInput.value,
+        };
+      }, {}) as Record<string, string>;
 
-    const expressionParts = expression.split('~');
+      const expressionParts = expression.split('~');
 
-    for (let i = 0; i < expressionParts.length; i++) {
-      const currentExpression = expressionParts[i];
+      for (let i = 0; i < expressionParts.length; i++) {
+        const currentExpression = expressionParts[i];
 
-      const currentExpressionParts = currentExpression.split('|');
-      const commands = currentExpressionParts[1].split('_');
+        const currentExpressionParts = currentExpression.split('|');
+        const commands = currentExpressionParts[1].split('_');
 
-      const currentInputName = currentExpressionParts[0];
-      const currentInputValue = inputsToValues[currentInputName];
+        const currentInputName = currentExpressionParts[0];
+        const currentInputValue = inputsToValues[currentInputName];
 
-      if (currentInputValue.includes('+')) {
-        continue;
-      }
-
-      if (currentExpressionParts[1].includes('eachWord')) {
-        const inputValueParts = currentInputValue.split(/-| /);
-
-        const valueToPush = this.mapCharsFromWords(
-          commands,
-          inputValueParts,
-          0,
-        );
-
-        resultEmailAddress.push(valueToPush);
-        resultEmailAddress.push('.');
-        //Skip the rest of the logic as we already completed the current iteration with input
-        continue;
-      }
-
-      if (currentExpressionParts[1].includes('lastWords')) {
-        const inputValueParts = currentInputValue.split(/-| /);
-
-        const valueToPush = this.mapCharsFromWords(
-          commands,
-          inputValueParts,
-          1,
-        );
-
-        resultEmailAddress.push(valueToPush);
-        resultEmailAddress.push('.');
-        //Skip the rest of the logic as we already completed the current iteration with input
-        continue;
-      }
-
-      if (
-        currentExpressionParts[1].includes('@') &&
-        !resultEmailAddress.includes('@')
-      ) {
-        if (resultEmailAddress[resultEmailAddress.length - 1] === '.') {
-          resultEmailAddress.pop();
+        if (currentInputValue.includes('+')) {
+          continue;
         }
 
-        resultEmailAddress.push('@');
-        continue;
+        if (currentExpressionParts[1].includes('eachWord')) {
+          const inputValueParts = currentInputValue.split(/-| /);
+
+          const valueToPush = this.mapCharsFromWords(
+            commands,
+            inputValueParts,
+            0,
+          );
+
+          resultEmailAddress.push(valueToPush);
+          resultEmailAddress.push('.');
+          //Skip the rest of the logic as we already completed the current iteration with input
+          continue;
+        }
+
+        if (currentExpressionParts[1].includes('lastWords')) {
+          const inputValueParts = currentInputValue.split(/-| /);
+
+          const valueToPush = this.mapCharsFromWords(
+            commands,
+            inputValueParts,
+            1,
+          );
+
+          resultEmailAddress.push(valueToPush);
+          resultEmailAddress.push('.');
+          //Skip the rest of the logic as we already completed the current iteration with input
+          continue;
+        }
+
+        if (
+          currentExpressionParts[1].includes('@') &&
+          !resultEmailAddress.includes('@')
+        ) {
+          if (resultEmailAddress[resultEmailAddress.length - 1] === '.') {
+            resultEmailAddress.pop();
+          }
+
+          resultEmailAddress.push('@');
+          continue;
+        }
+
+        if (
+          currentExpressionParts[1].length === 1 &&
+          currentExpressionParts[1] === '*'
+        ) {
+          if (i !== expressionParts.length - 1) {
+            resultEmailAddress.push(currentInputValue + '.');
+          } else {
+            resultEmailAddress.push(currentInputValue);
+          }
+          continue;
+        }
       }
 
-      if (
-        currentExpressionParts[1].length === 1 &&
-        currentExpressionParts[1] === '*'
-      ) {
-        if (i !== expressionParts.length - 1) {
-          resultEmailAddress.push(currentInputValue + '.');
-        } else {
-          resultEmailAddress.push(currentInputValue);
-        }
-        continue;
-      }
+      const finalOutput = resultEmailAddress.join('').toLowerCase();
+
+      return { data: [{ id: finalOutput, value: finalOutput }] };
+    } catch (e) {
+      console.log(
+        `Error attempting to generate email address: ${JSON.stringify(e)}`,
+      );
+      throw e;
     }
-
-    const finalOutput = resultEmailAddress.join('').toLowerCase();
-
-    return { data: [{ id: finalOutput, value: finalOutput }] };
   }
 
   private mapCharsFromWords(
