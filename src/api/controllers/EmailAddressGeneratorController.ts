20c812f4625d210745a4d7d042aa624ab1a84e8d
eduard vaklinov <eduard.vaklinov@abx.com>
2022-04-18 11:20:17 +0300

    Wrapped controller action in try catch block, added logging

--- src/api/controllers/EmailAddressGeneratorController.ts
+++ src/api/controllers/EmailAddressGeneratorController.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { EmailAddressGenerator } from '../../domain/services/EmailAddressGenerator';
 import {
   EmailAddressGeneratedResponse,
@@ -10,9 +10,21 @@ export class EmailAddressGeneratorController {
   constructor(private readonly emailAddressGenerator: EmailAddressGenerator) {}
 
   @Post('address')
+  @HttpCode(201)
   getHello(
     @Body() inputsRequest: EmailAddressGenerationRequest,
-  ): EmailAddressGeneratedResponse {
-    return this.emailAddressGenerator.generateEmailAddress(inputsRequest);
+  ): EmailAddressGeneratedResponse | { message: string } {
+    try {
+      const generatedEmailAddress =
+        this.emailAddressGenerator.generateEmailAddress(inputsRequest);
+      return generatedEmailAddress;
+    } catch (e) {
+      console.log(`${JSON.stringify(e)}`);
+      return {
+        message: `Failed to generate email address: ${JSON.stringify(
+          e.message,
+        )}`,
+      };
+    }
   }
 }
